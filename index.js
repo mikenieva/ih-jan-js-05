572ffa8f40f762dda1e746701b52c492736febfe
Mike Nieva <m@mikenieva.com>
2022-01-12 11:52:13 -0600

    Agregado de propiedades

--- index.js
+++ index.js
@@ -17,3 +17,36 @@ console.log(mexico.comida)
 // BRACKET NOTATION - NOTACIÓN POR CORCHETES
 console.log(mexico["comida"])
 
+const olympicRecords = {
+	athletics100Men: "Justin Gatlin",
+	athleticsLongJumpMen: "Mike Powel",
+	swimming200Men: "Michael Phelps",
+	"swimming 200 Women": "Ariarne Titmus"
+}
+
+console.log(olympicRecords.athletics100Men)
+console.log(olympicRecords["athletics100Men"])
+
+console.log(olympicRecords["swimming 200 Women"])
+
+
+// B. AGREGAR PROPIEDADES
+
+const refresco = {
+	nombre: "Coca cola",
+	color: "Oscuro",
+	precio: 12,
+	tamanos: [600, 1000]
+}
+
+refresco.envase = "Plástico"
+
+console.log("Objeto de refresco:", refresco)
+
+refresco["competidor"] = "Pepsi"
+
+console.log(refresco)
+
+
+
+
